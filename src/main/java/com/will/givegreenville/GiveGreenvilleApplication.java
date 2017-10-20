bed19a06a98c6a09f9c4d94bfbcbc61695e4273a
Will Bruce <willtiger13@gmail.com>
2017-10-20 17:26:56 -0400

    File upload boilerplate added into storage package. File Upload Controller also created.

--- src/main/java/com/will/givegreenville/GiveGreenvilleApplication.java
+++ src/main/java/com/will/givegreenville/GiveGreenvilleApplication.java
@@ -1,12 +1,26 @@
 package com.will.givegreenville;
 
+import com.will.givegreenville.storage.StorageProperties;
+import com.will.givegreenville.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class GiveGreenvilleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GiveGreenvilleApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 }
---
+++ src/main/java/com/will/givegreenville/storage/StorageProperties.java
@@ -0,0 +1,17 @@
+package com.will.givegreenville.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
